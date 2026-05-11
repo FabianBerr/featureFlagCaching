8b9d2da8d5c75724f9db4457093f09d08026d140
Fabian Berr <fabian.berr@syskron.com>
2026-05-11 08:34:08 +0200

    odatav2adapter (1.15.12) logic fix in middleware

--- srv/server.js
+++ srv/server.js
@@ -4,58 +4,49 @@ const xsenv = require("@sap/xsenv");
 xsenv.loadEnv();
 
 
-cds.on(
-    "bootstrap",
-    (app) =>
-    (cds.cov2ap.before = (req, res, next) => {
-        req.features = ["isbn"];
-        next();
-    }),
-);
-
-/* cds.middlewares.add(
+cds.middlewares.add(
     async function featureToggleMiddleware(req, _, next) {
         console.log(cds.env.profiles);
         aFeatures = [];
         //if (cds.env.profiles.includes('production')) {
         // if (cds.context.tenant === "002d05a8-0727-4da4-ab38-a31671726bc1" || cds.context.tenant === "tenant2") {
 
-        const services = xsenv.getServices({
-            featureflags: { tag: "feature-flags" },
-        });
+        if (cds.env.profiles.includes('production')) {
+            const services = xsenv.getServices({
+                featureflags: { tag: "feature-flags" },
+            });
 
-        const cache = await cds.connect.to("caching");
-        let oCachedFeatures = await cache.get("enabledFeatures");
-        if (!oCachedFeatures) {
-            const oFeatureFlagResponse = await fetch(`${services.featureflags.uri}/api/v1/features/export`,{
-                method: "GET",              
+            //const cache = await cds.connect.to("caching");
+            //let oCachedFeatures = await cache.get("enabledFeatures");
+            //if (!oCachedFeatures) {
+            const oFeatureFlagResponse = await fetch(`${services.featureflags.uri}/api/v1/features/export`, {
+                method: "GET",
                 headers: {
                     Authorization: `Basic ${Buffer.from(`${services.featureflags.username}:${services.featureflags.password}`).toString("base64")}`,
                 },
             });
 
-            
             if (!oFeatureFlagResponse.ok) {
                 throw new Error(`Feature flag request failed with status ${oFeatureFlagResponse.status}`);
             }
 
-             const oFeatureFlagData = await oFeatureFlagResponse.json();
+            const oFeatureFlagData = await oFeatureFlagResponse.json();
 
             if (oFeatureFlagData?.flags) {
                 aFeatures = oFeatureFlagData.flags
                     .filter((obj) => obj.enabled)
                     .map((obj) => obj.id);
+                //  }
+                //cache.set("enabledFeatures", aFeatures, { ttl: 60000 }); // cache for 1 minute
             }
-            cache.set("enabledFeatures", aFeatures, { ttl: 60000 }); // cache for 1 minute
-        }
-        else {
-            aFeatures = oCachedFeatures;
+            /*  else {
+                 aFeatures = oCachedFeatures;
+             } */
+            req.features = aFeatures;
+            cds.context.features = aFeatures;
         }
 
-        // }
-        req.features = aFeatures;
-        //}
         next();
     },
     { before: "ctx_model" },
-); */
+);
